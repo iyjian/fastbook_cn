9e6142b3c712652ed82def7d175083d1ab165d8b
w.c <52445078@qq.com>
2020-12-07 13:37:29 +0800

    fix

--- translate/scripts/extractSentence.js
+++ translate/scripts/extractSentence.js
@@ -3,8 +3,11 @@ const path = require('path')
 const moment = require('moment')
 const models = require('./../models')
 
+/**
+ * 抽取人工翻译的内容
+*/
 
-const main = async () => {
+const main = async (isDryRun) => {
 
   const BOOK_PATH = './../../'
 
@@ -37,17 +40,24 @@ const main = async () => {
                   console.log('错误！此行翻译未对应原文')
                   console.log(`章节：${chapterTitle} 章节序号：${chapterNum} 单元格：${row} 翻译内容：${paragraph}`)
                 } else {
-                  if (/^机器翻译:/.test(paragraph)) {
+                  if (/\[机器翻译\]\n{0,}$/.test(paragraph)) {
                     // 
+                    console.log(`machine translate rows. ignore it.`)
                   } else {
                     // 如果存在非机器翻译的翻译行，则把翻译行更新到原文行
-                    await models.book.update({
-                      manualTranslate: paragraph
-                    }, {
-                      where: {
-                        originParagraph: lastOriginParagraph
-                      }
-                    })
+                    if (isDryRun === 'true') {
+                      console.log('capture manuaTranslate scripts:')
+                      console.log(`originParagraph: ${lastOriginParagraph}`)
+                      console.log(`translated: ${paragraph}`)
+                    } else {
+                      await models.book.update({
+                        manualTranslate: paragraph
+                      }, {
+                        where: {
+                          originParagraph: lastOriginParagraph
+                        }
+                      })
+                    }
                   }
                 }
               } else {
@@ -60,13 +70,14 @@ const main = async () => {
                 if (isExist) {
                   // console.log('原文已经记录过！')
                 } else {
-                  await models.book.create({
-                    chapterNum,
-                    chapterTitle,
-                    originRowNum: row,
-                    originParagraph: paragraph,
-                    lastSnapshotDate: moment().format('YYYY-MM-DD HH:mm:ss')
-                  })
+                  console.log(`extractManualTranslate - findNewParagraph - chapter: ${chapterNum}.${chapterTitle} paragraph: ${paragraph}`)
+                  // await models.book.create({
+                  //   chapterNum,
+                  //   chapterTitle,
+                  //   originRowNum: row,
+                  //   originParagraph: paragraph,
+                  //   lastSnapshotDate: moment().format('YYYY-MM-DD HH:mm:ss')
+                  // })
                 }
                 lastOriginParagraph = paragraph
               }
@@ -82,4 +93,12 @@ const main = async () => {
 
 }
 
-main()
+const isDryRun = process.argv[2]
+
+if (isDryRun !== 'true' && isDryRun !== 'false') {
+  console.log(`you must specify isDryRun`)
+  process.exit(1)
+}
+
+process.exit(0)
+main(isDryRun)
